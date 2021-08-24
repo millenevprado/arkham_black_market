404bb214e5dd3c3b3dccf2aa985e3223c13da983
Raphael Cruz <raphacruz_90@hotmail.com>
2021-08-24 20:18:31 -0300

    routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,9 +4,9 @@ class OrdersController < ApplicationController
     @order.user = current_user
     @order.evil_artifacts = EvilArtifact.find(params[:evil_artifact_id])
     if order.save
-      redirect_to orders_path(@order)
+      redirect_to evil_artifact_orders_path(@order)
     else
-      redirect_to evil_artifact_path(@order)
+      redirect_to evil_artifact_path(@order.evil_artifacts)
     end
   end
 
