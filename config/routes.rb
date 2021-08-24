6557bd12e59099581f283fd01cdb1f5f72a19c2b
Raphael Cruz <raphacruz_90@hotmail.com>
2021-08-24 12:39:45 -0300

    back_home

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'evil_artifacts/index'
   devise_for :users
   root to: 'pages#home'
-  resources :evil_artifacts, only: :index
+  resources :evil_artifacts, only: :index do
+    collection do
+      get "/:category", to: "evil_artifacts#category", as: 'artifacts_of_a_category'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
