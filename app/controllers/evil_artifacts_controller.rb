ad7090dfc4a3e952a71de4021312c5602fd89de6
Giovanna Leite Nunes Coelho <giovanna.lnc02@gmail.com>
2021-08-24 18:27:15 -0300

    route to list all categories and route to list all the products off a category done

--- app/controllers/evil_artifacts_controller.rb
+++ app/controllers/evil_artifacts_controller.rb
@@ -1,5 +1,12 @@
 class EvilArtifactsController < ApplicationController
   def index
-    @evil_artifacts = EvilArtifact.all
+    @evil_categories = EvilArtifact::CATEGORIES
+  end
+
+  def artifacts_of_a_category
+    @evil_artifacts = EvilArtifact.where(category: params[:category])
+  end
+
+  def show
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :evil_artifacts, only: :index do
+  resources :evil_artifacts, only: [:index, :show] do
     collection do
-      get "/:category", to: "evil_artifacts#category", as: 'artifacts_of_a_category'
+      get "/category/:category", to: "evil_artifacts#artifacts_of_a_category", as: 'artifacts_of_a_category'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
