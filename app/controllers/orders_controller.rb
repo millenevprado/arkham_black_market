d5e100b0311dd835f187da25f22f7266ff1a4b0f
Raphael Cruz <raphacruz_90@hotmail.com>
2021-08-24 16:44:44 -0300

    orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,22 +1,12 @@
 class OrdersController < ApplicationController
-
-  def new
-    @order = Order.new
-  end
-
   def create
-    @order = Order.new(order_params)
-    @bookmark.list = @list
+    @order = Order.new
+    @order.user = current_user
+    @order.evil_artifacts = EvilArtifact.find(params[:evil_artifact_id])
     if order.save
-      redirect_to list_path(@list)
+      redirect_to orders_path(@order)
     else
       render :new
     end
-  end
-
-  private
-
-  def order_params
-    params.require(:order).permit(:user_id, :evil_artifacts_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   get 'evil_artifacts/index'
   devise_for :users
   root to: 'pages#home'
-  resources :evil_artifacts, only: :index
-  resources :orders, only: [:new, :create]
+  resources :evil_artifacts, only: :index do
+    resources :orders, only: :create
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
