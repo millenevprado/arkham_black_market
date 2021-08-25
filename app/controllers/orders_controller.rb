37869c4da82de23c77d75668d5446fb9efaeb904
Raphael Cruz <raphacruz_90@hotmail.com>
2021-08-25 15:07:59 -0300

    order done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,31 @@
 class OrdersController < ApplicationController
-  def create
+  def new
     @order = Order.new
+    @evil_artifact = EvilArtifact.find(params[:evil_artifact_id])
+  end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def create
+    @order = Order.new(order_params)
     @order.user = current_user
-    @order.evil_artifacts = EvilArtifact.find(params[:evil_artifact_id])
-    if order.save
-      redirect_to evil_artifact_orders_path(@order)
+    evil_artifact = EvilArtifact.find(params[:evil_artifact_id])
+    @order.evil_artifact = evil_artifact
+    if @order.save
+      redirect_to order_path(@order)
     else
-      redirect_to evil_artifact_path(@order.evil_artifacts)
+      redirect_to evil_artifact_path(@order.evil_artifact)
     end
   end
 
   def update
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:quantity)
   end
 end
