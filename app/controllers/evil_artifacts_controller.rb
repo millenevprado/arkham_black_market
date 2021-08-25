810da2d4a70d968cb4111fa4cdde4b37104888e5
Giovanna Leite Nunes Coelho <giovanna.lnc02@gmail.com>
2021-08-25 14:00:14 -0300

    create edit update delete done with basic security

--- app/controllers/evil_artifacts_controller.rb
+++ app/controllers/evil_artifacts_controller.rb
@@ -10,4 +10,45 @@ class EvilArtifactsController < ApplicationController
   def show
     @evil_artifact = EvilArtifact.find(params[:id])
   end
+
+  def new
+    @evil_artifact = EvilArtifact.new
+  end
+
+  def create
+    @evil_artifact = EvilArtifact.new(evil_artifact_params)
+    @evil_artifact.user = current_user
+    if @evil_artifact.save
+      redirect_to evil_artifact_path(@evil_artifact), notice: 'Evil artifact was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @evil_artifact = EvilArtifact.find(params[:id])
+  end
+
+  def update
+    @evil_artifact = EvilArtifact.find(params[:id])
+    if @evil_artifact.user == current_user
+      @evil_artifact.update(evil_artifact_params)
+      flash[:notice] = 'Updated!'
+    else
+      flash[:notice] = 'Not authorized!'
+    end
+    redirect_to evil_artifact_path(@evil_artifact)
+  end
+
+  def destroy
+    @evil_artifact = EvilArtifact.find(params[:id])
+    @evil_artifact.destroy if @evil_artifact.user == current_user
+    redirect_to evil_artifacts_path
+  end
+
+  private
+
+  def evil_artifact_params
+    params.require(:evil_artifact).permit(:name, :description, :price, :category)
+  end
 end
