f4e4efe240047b8dbe9e424d7c2f9775ccf2b195
Raphael Cruz <raphacruz_90@hotmail.com>
2021-08-26 15:31:12 -0300

    implement photos upload

--- app/models/evil_artifact.rb
+++ app/models/evil_artifact.rb
@@ -1,10 +1,11 @@
 class EvilArtifact < ApplicationRecord
   belongs_to :user
+  has_many :orders, dependent: :destroy
   has_many :users, through: :orders
   has_many_attached :photos
 
   CATEGORIES = ['Poison', 'Fire Guns', 'Meele Weapon', 'Hallucinogenic', 'Explosives', 'Pull or Prank'].freeze
-  validates :name, :description, :price, :category, presence: true
+  validates :name, :description, :price, :category, :photos, presence: true
   validates :description, length: { minimum: 10 }
   validates :price, numericality: { only_float: true }
   validates :category, inclusion: { in: CATEGORIES }
