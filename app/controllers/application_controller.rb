73bde4d894ee95dd6424ef3e8f48089e8af5fa09
Giovanna Leite Nunes Coelho <giovanna.lnc02@gmail.com>
2021-08-26 17:12:33 -0300

    user photo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:codename])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:codename, :photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: [:codename])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:codename, :photo])
   end
 end
